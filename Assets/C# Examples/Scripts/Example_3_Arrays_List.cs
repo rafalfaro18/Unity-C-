4b0ca6566a95d5297b6b0c516bbc1172cc3b5afc
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-11 22:03:16 -0600

    debugs array value

---
+++ Assets/C# Examples/Scripts/Example_3_Arrays_List.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example_3_Arrays_List : MonoBehaviour {
+
+	string[] playerNames = new string[3];
+
+	// Use this for initialization
+	void Start () {
+		playerNames[0] = "john";
+		playerNames[1] = "jane";
+		playerNames[2] = "bob";
+
+		Debug.Log("Player name is"+playerNames[1]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
