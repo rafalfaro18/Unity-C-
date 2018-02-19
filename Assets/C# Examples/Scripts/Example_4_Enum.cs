2a38cbaded78b7213427a583cc6f8d2b415d1e37
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-18 22:54:35 -0600

    Enum example

--- Assets/C# Examples/Scripts/Example_4_Enum.cs
+++ Assets/C# Examples/Scripts/Example_4_Enum.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Example_4_Enum : MonoBehaviour {
 
+	public enum LevelOfDifficulty { easy, medium, hard, difficult };
+
+	public LevelOfDifficulty levelOfDifficulty = LevelOfDifficulty.easy;
+
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log("starting level of difficulty: " + levelOfDifficulty);
+		levelOfDifficulty = LevelOfDifficulty.hard;
+		Debug.Log("starting level of difficulty" + levelOfDifficulty);
 	}
 	
 	// Update is called once per frame
