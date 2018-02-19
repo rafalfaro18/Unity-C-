b94cdcffbee081a51b1cd46a43d92c6d6027b68d
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-18 21:21:57 -0600

    sets up example 4

---
+++ Assets/C# Examples/Scripts/Example_4_Enum.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example_4_Enum : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
